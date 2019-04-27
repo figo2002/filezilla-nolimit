1e8edc0d87f7e7fd0fc0c1e45bd15e759d056510
figo2002 <figo2002@users.noreply.github.com>
2019-04-27 21:12:12 +0800

    Update deps.sh

--- deps.sh
+++ deps.sh
@@ -1,10 +1,7 @@
 #/usr/bin/env sh
 mkdir ~/prefix
 mkdir ~/src
-export PATH="$HOME/prefix/bin:$PATH"
-export LD_LIBRARY_PATH="$HOME/prefix/lib:$LD_LIBRARY_PATH"
-export PKG_CONFIG_PATH="$HOME/prefix/lib/pkgconfig:$PKG_CONFIG_PATH"
-export TARGET_HOST=x86_64-w64-mingw32
+
 
 cd ~/src
 wget https://gmplib.org/download/gmp/gmp-6.1.2.tar.lz
