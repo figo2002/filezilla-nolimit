d3587a9f565b0ccff9be95124c1a71fc7c64806c
figo2002 <figo2002@users.noreply.github.com>
2019-04-27 12:39:19 +0800

    Delete build_env.sh

--- build_env.sh
+++
@@ -1,5 +0,0 @@
-#/usr/env/bin sh
-dpkg --add-architecture i386
-apt update
-apt install automake autoconf libtool make gettext lzip
-apt install mingw-w64 pkg-config wx-common wine wine32 subversion git
