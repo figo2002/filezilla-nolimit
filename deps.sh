c9d7871a57edc674b25645033127b9819fcac43b
figo2002 <figo2002@users.noreply.github.com>
2019-04-27 19:37:04 +0800

    Update deps.sh

--- deps.sh
+++ deps.sh
@@ -10,8 +10,7 @@ cd ~/src
 wget https://gmplib.org/download/gmp/gmp-6.1.2.tar.lz
 tar xf gmp-6.1.2.tar.lz
 cd gmp-6.1.2
-./configure --host=$TARGET_HOST --prefix="$HOME/prefix" --disable-static 
---enable-shared --enable-fat
+./configure --host=$TARGET_HOST --prefix="$HOME/prefix" --disable-static --enable-shared --enable-fat
 make
 make install
 
@@ -19,8 +18,7 @@ cd ~/src
 wget https://ftp.gnu.org/gnu/nettle/nettle-3.4.1.tar.gz
 tar xf nettle-3.4.1.tar.gz
 cd nettle-3.4.1
-./configure --host=$TARGET_HOST --prefix="$HOME/prefix" --enable-shared 
---disable-static --enable-fat LDFLAGS="-L$HOME/prefix/lib" 
+./configure --host=$TARGET_HOST --prefix="$HOME/prefix" --enable-shared --disable-static --enable-fat LDFLAGS="-L$HOME/prefix/lib" 
 CPPFLAGS="-I$HOME/prefix/include"
 make
 make install
@@ -30,9 +28,7 @@ cd ~/src
 wget https://www.gnupg.org/ftp/gcrypt/gnutls/v3.6/gnutls-3.6.6.tar.xz
 tar xvf gnutls-3.6.6.tar.xz
 cd gnutls-3.6.6
-./configure --host=$TARGET_HOST --prefix="$HOME/prefix" --enable-shared 
---disable-static --with-included-libtasn1 --without-p11-kit 
---disable-doc --enable-local-libopts --with-included-unistring 
+./configure --host=$TARGET_HOST --prefix="$HOME/prefix" --enable-shared --disable-static --with-included-libtasn1 --without-p11-kit --disable-doc --enable-local-libopts --with-included-unistring 
 GMP_LIBS="-L$HOME/prefix/lib -lgmp" GMP_CFLAGS="-I$HOME/prefix/include"
 make
 make install
@@ -42,8 +38,7 @@ cd ~/src
 wget https://sqlite.org/2018/sqlite-autoconf-32600-00.tar.gz
 tar xvzf sqlite-autoconf-3260000.tar.gz
 cd sqlite-autoconf-3260000
-./configure --host=$TARGET_HOST --prefix="$HOME/prefix" --enable-shared 
---disable-static --disable-dynamic-extensions
+./configure --host=$TARGET_HOST --prefix="$HOME/prefix" --enable-shared --disable-static --disable-dynamic-extensions
 make
 make install
 
@@ -51,8 +46,7 @@ cd ~/src
 git clone --branch WX_3_0_BRANCH --single-branch 
 https://github.com/wxWidgets/wxWidgets.git wx3
 cd wx3
-./configure --host=$TARGET_HOST --prefix="$HOME/prefix" --enable-shared 
---disable-static
+./configure --host=$TARGET_HOST --prefix="$HOME/prefix" --enable-shared --disable-static
 make
 make install
 
